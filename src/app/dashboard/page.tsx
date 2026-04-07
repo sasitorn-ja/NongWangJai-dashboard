31d07fb1546b3d72ca059c3d38adad4ca86e5355
Sasitorn <sasitorn@MacBook-Pro-khxng-Sasitorn.local>
2026-04-07 10:58:12 +0700

    update

--- src/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-// import { DashboardMain } from "@/components/dashboard/dashboard";
-
-// export default function DashboardPage() {
-//   return (
-//     <main className="min-h-screen bg-pt-neutral-50">
-//       <DashboardMain />
-//     </main>
-//   );
-// }
